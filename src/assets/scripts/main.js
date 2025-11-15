1d946b306d98778bfb0df6c8e0aa4da5dc10aac7
daviddm1980 <daviddm1980@outlook.com>
2025-11-15 07:45:48 +0100

    algo de estilos

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,20 +1,31 @@
 
 
 const textArea = document.getElementById('toDo');
-const containerToDo = document.getElementById('toDoContainer');
 
 function crearTarea() {
-    const valorArea = textArea.value;
-
+    const valorArea = textArea.value.trim();
+    
     if (textArea.value === '') return;
-
+    
+    const containerToDo = document.querySelector('ul');
+    const li = document.createElement('li');
     const tareaElement = document.createElement('p');
     tareaElement.classList.add('tarea');
-    tareaElement.textContent = valorArea.trim();
-    containerToDo.appendChild(tareaElement);
+    tareaElement.textContent = valorArea;
+
+    const times = document.createElement('span');
+    times.classList.add('times');
+    times.textContent = 'X';
+
+    times.addEventListener('click',()=> {
+        li.remove();
+    });
+    // li.appendChild()
+    li.appendChild(times);
+    containerToDo.appendChild(li);
+    li.appendChild(tareaElement);
 
     textArea.value = '';
 }
-
 
 document.getElementById('btnEnviar').addEventListener('click',crearTarea);

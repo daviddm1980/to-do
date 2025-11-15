56a08255654f498a2c23a5e73bbcfe90bc40d55d
daviddm1980 <daviddm1980@outlook.com>
2025-11-15 07:55:59 +0100

    prettier

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,31 +1,29 @@
-
-
-const textArea = document.getElementById('toDo');
+const textArea = document.getElementById("toDo");
 
 function crearTarea() {
-    const valorArea = textArea.value.trim();
-    
-    if (textArea.value === '') return;
-    
-    const containerToDo = document.querySelector('ul');
-    const li = document.createElement('li');
-    const tareaElement = document.createElement('p');
-    tareaElement.classList.add('tarea');
-    tareaElement.textContent = valorArea;
+	const valorArea = textArea.value.trim();
 
-    const times = document.createElement('span');
-    times.classList.add('times');
-    times.textContent = 'X';
+	if (textArea.value === "") return;
 
-    times.addEventListener('click',()=> {
-        li.remove();
-    });
-    // li.appendChild()
-    li.appendChild(times);
-    containerToDo.appendChild(li);
-    li.appendChild(tareaElement);
+	const containerToDo = document.querySelector("ul");
+	const li = document.createElement("li");
+	const tareaElement = document.createElement("p");
+	tareaElement.classList.add("tarea");
+	tareaElement.textContent = valorArea;
 
-    textArea.value = '';
+	const times = document.createElement("span");
+	times.classList.add("times");
+	times.textContent = "X";
+
+	times.addEventListener("click", () => {
+		li.remove();
+	});
+	// li.appendChild()
+	li.appendChild(times);
+	containerToDo.appendChild(li);
+	li.appendChild(tareaElement);
+
+	textArea.value = "";
 }
 
-document.getElementById('btnEnviar').addEventListener('click',crearTarea);
+document.getElementById("btnEnviar").addEventListener("click", crearTarea);
